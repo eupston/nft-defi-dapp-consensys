7d8d0e06941876de7eb73df92ccef5c6cf384499
Eug <eupston130@hotmail.com>
2024-04-09 15:16:22 +0800

    wagmi for approve and takeloan

--- components/LoanNFTButton.tsx
+++ components/LoanNFTButton.tsx
@@ -1,36 +1,47 @@
 import { parseEther } from "ethers";
 import { Button } from "./ui/button";
 import { useState } from "react";
-import { useContracts } from "@/lib/hooks/useContracts";
+import { writeContract, waitForTransactionReceipt } from "@wagmi/core";
+import { config } from "@/wagmi";
+import constants from "@/lib/constants";
 
 interface LoanNFTButtonProps {
   tokenId: number;
 }
 
 export const LoanNFTButton: React.FC<LoanNFTButtonProps> = ({ tokenId }) => {
-  const { signer, nftCollateralizer, mintable } = useContracts();
   const [isNFTApproved, setIsNFTApproved] = useState(false);
   const [isLoading, setIsLoading] = useState(false); // New loading state
-
   const loanNFT = async () => {
-    if (nftCollateralizer && signer && mintable) {
-      setIsLoading(true);
-      try {
-        const loanAmount = parseEther("0.001");
-        if (!isNFTApproved) {
-          const address = await nftCollateralizer.getAddress();
-          const approveTx = await mintable.approve(address, tokenId);
-          await approveTx.wait();
-          setIsNFTApproved(true);
-        } else {
-          const tx = await nftCollateralizer.takeLoan(tokenId, loanAmount);
-          await tx.wait();
-        }
-      } catch (error) {
-        console.error("Transaction cancelled or failed", error);
-      } finally {
-        setIsLoading(false);
+    setIsLoading(true);
+    try {
+      const loanAmount = parseEther("0.001");
+      if (!isNFTApproved) {
+        const results = await writeContract(config, {
+          address: constants.sepolia.MINTABLENFT as `0x${string}`,
+          abi: constants.NFT_ABI.abi,
+          functionName: "approve",
+          args: [constants.sepolia.NFTCOLLATERALLOAN, tokenId],
+        });
+        await waitForTransactionReceipt(config, {
+          hash: results,
+        });
+        setIsNFTApproved(true);
+      } else {
+        const results = await writeContract(config, {
+          address: constants.sepolia.NFTCOLLATERALLOAN as `0x${string}`,
+          abi: constants.LOAN_ABI.abi,
+          functionName: "takeLoan",
+          args: [tokenId, loanAmount],
+        });
+        await waitForTransactionReceipt(config, {
+          hash: results,
+        });
       }
+    } catch (error) {
+      console.error("Transaction cancelled or failed", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
